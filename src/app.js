bd8a676289ca664877239e8df585e80c407e2d53
claud-car <claudiocarchietti@yahoo.it>
2021-04-23 13:10:37 +0200

    Inserendo funzionalità Vue

---
+++ src/app.js
@@ -0,0 +1,54 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+        textInsertJumbo:[
+            {
+                jumbo:'HOME'
+            },
+            {
+                jumbo:'ABOUT'
+            },
+            {
+                jumbo:'SERVICES'
+            },
+            {
+                jumbo:'PROCESS'
+            },
+            {
+                jumbo:'TEAM'
+            },
+            {
+                jumbo:'BLOG'
+            },
+        ],
+        buttonText:[
+            {
+                button: "GET IN TOUCH"
+            }
+        ],
+        teamExpert:[
+            {
+                img: "./img/team-1.jpg",
+                name: "David Cooper",
+                category: "CTO & CO-FOUNDER"
+            },
+            {
+                img: "./img/team-2.jpg",
+                name: "Emma Lopez",
+                category: "CHIEF MARKETING"
+            },
+            {
+                img: "./img/team-3.jpg",
+                name: "Oliver Jones",
+                category: "CHIEF PROCUREMENT"
+            },
+            {
+                img: "./img/team-4.jpg",
+                name: "T.Johnson",
+                category: "CEO & PRESIDENT"
+            }
+        ]
+    }
+}) //fine root
+
+console.log("bababaab");
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,18 @@ let mix = require('laravel-mix');
 
 // mix.sass('src/scss/master.scss', 'assets/');
 
-mix.sass('src/scss/master.scss', 'assets').options({
+mix.js('src/app.js', 'dist/js')
+.sass('src/scss/master.scss', 'assets')
+.setPublicPath('dist')
+.options({
     processCssUrls: false
 });
+
+
+// let mix = require('laravel-mix');
+//  mix.js('src/js/main.js', 'js')
+//  .sass('src/master.scss','css')
+//  .setPublicPath('assets')
+//  .options({
+//     processCssUrls: false
+// });
